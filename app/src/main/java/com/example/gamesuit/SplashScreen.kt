d1069f72ef1f67cc7d8f46c57926b3234fae3753
widarsono <widarsono@rizkawidarsono29@gmail.com>
2020-06-19 00:58:33 +0700

    Menambahkan Splash Screen

---
+++ app/src/main/java/com/example/gamesuit/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.gamesuit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        val beckground = object : Thread(){
+            override fun run() {
+                try {
+                    Thread.sleep(2000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        beckground.start()
+    }
+}
